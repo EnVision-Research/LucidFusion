b521412ea9770f82b4696b62c31cb9520db681bc
Dreamdragon <heye0507@gmail.com>
2024-10-17 19:23:41 -0700

    Update demo.sh
    update demo.sh

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 YAML="options/demo.yaml"
-DEMO='/hpc2hdd/home/hheat/projects/Lucidfusion/examples/nutella_new'
-CKPT='/hpc2hdd/home/hheat/projects/Lucidfusion/output/demo/best.ckpt'
-OUTPUT='/hpc2hdd/home/hheat/projects/Lucidfusion/output/res'
+DEMO='examples/nutella_new'
+CKPT='output/demo/best.ckpt'
+OUTPUT='output/demo'
 export CUDA_VISIBLE_DEVICES='0'
 python demo.py --yaml=$YAML --task=demo --data.demo_path=$DEMO --ckpt=$CKPT --save_path=$OUTPUT \
 --save_video=true --save_per_view_ply=false --lucid_cam=false \
